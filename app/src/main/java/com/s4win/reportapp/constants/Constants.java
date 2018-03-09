6984b0558b4b8e71b042c9f8eb884c724fc56e93
Dario Pellegrini <pellegrini.dario.1303@gmail.com>
2018-03-09 17:55:45 +0100

    URL

--- app/src/main/java/com/s4win/reportapp/constants/Constants.java
+++ app/src/main/java/com/s4win/reportapp/constants/Constants.java
@@ -5,6 +5,6 @@ package com.s4win.reportapp.constants;
  */
 
 public class Constants {
-    public static final String BASE_URL = "https://s4windemoapp.stamplayapp.com/";
+    public static final String BASE_URL = "http://localhost/";
     public static final String REPORTS_ENPOINT = "api/cobject/v1/report";
 }
